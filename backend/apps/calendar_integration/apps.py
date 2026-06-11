595b9a0710fdf0dd05824eec99fdbe27df603a30
Taher <taher@leadflow.ai>
2026-06-11 05:03:30 +0300

    feat(calendar): add app config and package init

---
+++ backend/apps/calendar_integration/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CalendarIntegrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.calendar_integration'
+    verbose_name = 'Calendar Integration'
